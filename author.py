a11cf799d7f1b3b448af8170b288b9a2e38bdc98
dcarleton67 <45353625+dcarleton67@users.noreply.github.com>
2018-12-03 14:42:51 +0800

    Update author.py

--- author.py
+++ author.py
@@ -1,1 +1,12 @@
-
+class Author:
+  def __init__(self, authorName):
+    """Initializes Author class"""
+    self.authorName = authorName
+    self.wordCount = 0
+    self.wordFrequency = {}
+    
+  def countWords(self, story):
+    """Parses a story and counts each word in said dictionary"""
+    
+  def evaluate(self, story):
+    """Parses a story and gives a score based on likelihood of this being the author"""
