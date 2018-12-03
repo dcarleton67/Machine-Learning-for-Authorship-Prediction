bc2aed568b0c08b06df124fb16e66de8cbbeaedb
dcarleton67 <45353625+dcarleton67@users.noreply.github.com>
2018-12-03 14:44:45 +0800

    Update author.py

--- author.py
+++ author.py
@@ -1,12 +1,19 @@
 class Author:
-  def __init__(self, authorName):
+  def __init__(self, authorName, filterLength):
     """Initializes Author class"""
     self.authorName = authorName
     self.wordCount = 0
     self.wordFrequency = {}
     
+  def filter(self, story):
+    """Parses a story and filters out words greater than filterLength"""
+    return NotImplemented
+    
+    
   def countWords(self, story):
     """Parses a story and counts each word in said dictionary"""
+    return NotImplemented
     
   def evaluate(self, story):
     """Parses a story and gives a score based on likelihood of this being the author"""
+    return NotImplemented
