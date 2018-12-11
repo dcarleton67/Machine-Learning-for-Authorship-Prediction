38a48fc9716dcf31191235c4feb65dd36e8d0a3c
ASAPLukeP <39285339+ASAPLukeP@users.noreply.github.com>
2018-12-11 14:20:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 #This is the test file
 #need to bring in data
 from Author import Author
+from Author import predict
+from Author import eliminate
 def bringindata():
     #f = gzip.open('C:/Users/lmip/Documents/VE593/Project3/mnist.pkl.gz', 'rb')
     first = Author("apress", 5)
     second = Author("acrosby", 5)
-    third = Author("next", 5)
-    filteredone = first.filter("C:/Users/lmip/Documents/VE593/Project4/apresstrain.txt")
+    third = Author("BK", 5)
+    four = Author("BH",5)
+    five = Author("AS", 5)
+    filteredone = first.filter("C:/Users/lmip/Documents/VE593/Project4/APtrain.txt")
     first.countWords(filteredone)
-    filteredtwo = second.filter("C:/Users/lmip/Documents/VE593/Project4/ACrosbytrain.txt")
+    filteredtwo = second.filter("C:/Users/lmip/Documents/VE593/Project4/ACtrain.txt")
     second.countWords(filteredtwo)
+    filteredthree = third.filter("C:/Users/lmip/Documents/VE593/Project4/BKtrain.txt")
+    third.countWords(filteredthree)
+    filteredfour = four.filter("C:/Users/lmip/Documents/VE593/Project4/BHtrain.txt")
+    four.countWords(filteredfour)
+    fivedat = five.filter("C:/Users/lmip/Documents/VE593/Project4/AStrain.txt")
+    five.countWords(fivedat)
 
-    filtered = first.filter("C:/Users/lmip/Documents/VE593/Project4/ACrosbytest.txt")
+    filtered = first.filter("C:/Users/lmip/Documents/VE593/Project4/APtest.txt")
+    filtered1 = first.filter("C:/Users/lmip/Documents/VE593/Project4/ACtest.txt")
+    filtered2 = first.filter("C:/Users/lmip/Documents/VE593/Project4/AStest.txt")
+    filtered3 = first.filter("C:/Users/lmip/Documents/VE593/Project4/BKtest.txt")
+    filtered4 = first.filter("C:/Users/lmip/Documents/VE593/Project4/BHtest.txt")
+    stories = [filtered, filtered1, filtered2, filtered3, filtered4]
+
+
     total = 0
-    for it in range(0,len(filtered),100):
-        print(len(filtered))
-        score2 = second.evaluate(filtered[it:it+100])
-        score = first.evaluate(filtered[it:it+100])
-        #print(score)
-        #print(score2)
-        print(score2>score)
-        if (score2>score):
-            total+=1
-    print(total)
-
+    authors = [first, second, third, four, five]
+    #authors = [ second, four, five]
+    eliminate(authors,stories)
+   
 
 
     return total
